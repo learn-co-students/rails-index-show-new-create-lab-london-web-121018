6ddcde966f262cbd7fdfc798c312a13f476af650
amaan19 <amaan.ahmed3@gmail.com>
2019-01-14 09:51:33 +0000

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,24 @@
+class CouponsController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def create
+
+    @coupon = Coupon.new
+
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+end
